ae7d71ca4c2c26457cdffa542166b5d06ef10523
QusaiAlbonni <axinkiro@gmail.com>
2024-11-26 19:22:13 +0300

    corrected misconfigured backend build command

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python QuakStore/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python QuakStore/manage.py migrate
+python manage.py migrate
