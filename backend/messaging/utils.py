25b3cda2c4f2c5ebeac41663d97ac9335ed2fbda
QusaiAlbonni <axinkiro@gmail.com>
2024-12-17 12:05:18 +0300

    replaced langdetect with lingua

--- backend/messaging/utils.py
+++ backend/messaging/utils.py
@@ -2,10 +2,14 @@ from asgiref.sync import sync_to_async
 from langdetect import detect
 from .models import Message
 
+from lingua import Language, LanguageDetectorBuilder
+languages = Language.all()
+detector = LanguageDetectorBuilder.from_languages(*languages).build()
+
 
 @sync_to_async
 def detect_lang(text: str):
-    return detect(text)
+    return detector.detect_language_of(text).iso_code_639_1.name.lower()
 
 async def detect_message_lang(message: Message):
     return await detect_lang(text=message.content)
